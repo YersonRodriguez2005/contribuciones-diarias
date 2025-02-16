374c31f620dcd5e43d84eefb1d8c57ee3e44e013
Yerson Rodriguez <rodriguezyerson2005@gmail.com>
2025-02-16 10:04:29 -0500

    update

--- commit_diario.bat
+++ commit_diario.bat
@@ -1,15 +1,8 @@
 @echo off
 cd C:\Users\YERSON\Documents\GitHub\contribuciones-diarias
-
-:: Obtener la fecha en formato YYYY-MM-DD
 for /f "tokens=2 delims==" %%I in ('"wmic os get localdatetime /value"') do set datetime=%%I
 set fecha=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2%
-
-:: Agregar la fecha al archivo contribucion.txt
-echo Contribución diaria %fecha% - %time% >> contribucion.txt
-
-:: Hacer commit y push
+echo Contribución diaria %fecha% >> contribucion.txt
 git add contribucion.txt
 git commit -m "Contribución diaria %fecha%"
-git pull origin main --rebase
 git push origin main
